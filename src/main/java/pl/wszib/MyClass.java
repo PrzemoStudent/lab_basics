0c4e00c3ab26e0f374516b33a02af26d3794e1cf
PrzemoStudent <hladasz@student.wszib.edu.pl>
2021-10-02 18:53:11 +0200

    Class

---
+++ src/main/java/pl/wszib/MyClass.java
@@ -0,0 +1,17 @@
+package pl.wszib;
+
+public class MyClass extends MyAbstractClass implements MyInterface, MyInterface2{
+    @Override
+    public void doCalculation(){
+        doPrint();
+    }
+    @Override
+    public void doRun(){
+
+    }
+    @Override
+    public void doRun2(){
+
+    }
+
+}
